d27b2876ac6f251c937f0c03ef9a89ad40a637ce
HafizMudassirHusain <hmudassir511@gmail.com>
2024-07-21 13:04:38 -0700

    modify btns

--- E-Commerse/Admin/admin.js
+++ E-Commerse/Admin/admin.js
@@ -17,7 +17,7 @@ products.push(obj)
 localStorage.setItem('menucard',JSON.stringify(products))
 }
 var convert = JSON.parse(localStorage.getItem('menucard'))
-console.log(convert)
+
 convert.forEach((data) => {
   // console.log(data)
   getmenu.innerHTML += `<div class="card" style="width: 18rem;">
@@ -28,10 +28,53 @@ convert.forEach((data) => {
     <p class="card-text">${data.description}</p>
     <h5 class="card-title">${data.price}</h5>
     <button class="btn btn-primary">EdiT btn</button>
-    <button class="btn btn-primary">Delete btn</button>
+    <button class="btn btn-primary" onclick="delt(this)">Delete btn</button>
   </div>
 </div>`;
 });
-
-
 // ======================================================
+function delt(data){
+data.parentNode.parentNode.remove();
+}
+
+
+function view1(){
+  var view = document.getElementById('callingdiv')
+  view.childNodes[1].style.display ='block';
+  view.childNodes[3].style.display ='none';
+  view.childNodes[5].style.display ='none';
+  view.childNodes[7].style.display ='none';
+  view.childNodes[9].style.display ='none';
+}
+function view2(){
+  var view = document.getElementById('callingdiv')
+  view.childNodes[1].style.display ='none';
+  view.childNodes[3].style.display ='block';
+  view.childNodes[5].style.display ='none';
+  view.childNodes[7].style.display ='none';
+  view.childNodes[9].style.display ='none';
+}
+function view3(){
+  var view = document.getElementById('callingdiv')
+  view.childNodes[1].style.display ='none';
+  view.childNodes[3].style.display ='none';
+  view.childNodes[5].style.display ='block';
+  view.childNodes[7].style.display ='none';
+  view.childNodes[9].style.display ='none';
+}
+function view4(){
+  var view = document.getElementById('callingdiv')
+  view.childNodes[1].style.display ='none';
+  view.childNodes[3].style.display ='none';
+  view.childNodes[5].style.display ='none';
+  view.childNodes[7].style.display ='block';
+  view.childNodes[9].style.display ='none';
+}
+function view5(){
+  var view = document.getElementById('callingdiv')
+  view.childNodes[1].style.display ='none';
+  view.childNodes[3].style.display ='none';
+  view.childNodes[5].style.display ='none';
+  view.childNodes[7].style.display ='none';
+  view.childNodes[9].style.display ='block';
+}
